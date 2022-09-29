9ab0a27f86acde7762009bb27f30da161e8c3bd8
Amine <asbaay@iesebre.com>
2022-09-29 14:07:27 +0200

    first commit

---
+++ UF1/activitat1@Sbaay_Amine (1).py
@@ -0,0 +1,35 @@
+'''1.1'''
+'''numero1 = int(input("Donam un numero: \n"))
+numero2 = int(input("Donam un segon numero: \n"))
+
+suma = (numero1 + numero2)
+
+resultat = (suma/2)
+
+print("La mitjana aritmerica des numeros que m'has donat es " + str(resultat))
+'''
+'''1.2
+altura = float(input("Cuan medeixes?: "))
+pes = int(input("Cuan peses?: "))
+
+index = ((pes)/altura**2)
+indexarrodonit = round(index,2)
+print("El index de la teva massa corporal es de " , indexarrodonit)
+'''
+'''1.3
+celsius = int(input("Quina temperatura a en celsius?: "))
+
+farenheid = ((celsius * 1.8) + 32)
+farenheidarrodonit = round(farenheid,0)
+print(str(celsius) +" Graus celsius en farenheid es de " , farenheidarrodonit)
+'''
+'''1.4'''
+hora = int(input("Quina hora es?\n"))
+minuts = int(input("Quin minut es?\n"))
+
+h=(hora*3600)
+m=(minuts*60)
+segons = (m+h)
+print("El total de segons es" , str(segons))
+
+'''DESCOMENTA CADA ACTIVITAT I COMENTA LES ALTRES SI  VOLS PROBAR ALGUNA SIUUU'''
